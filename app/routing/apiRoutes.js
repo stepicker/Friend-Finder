ac59d39259ba4df3a17f037934710d64815548ec
Picker <picker@gmail.com>
2019-02-15 16:22:46 -0500

    Final touches

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,13 +29,15 @@ module.exports = function(app) {
         for (var i = 0; i < friendsArray.length; i++) {
     
             var userDifference = 0;
+
+            console.log("----------\nCalculating difference with friend #", (i + 1));
         
             // For each existing friend, calculate the score difference with newFriend by looping through the 10 scores
             for (var j = 0; j < friendsArray[i].scores.length; j++) {
                 userDifference += Math.abs(friendsArray[i].scores[j] - newFriend.scores[j]);
-                console.log("Loop # ", (j + 1), " | user difference: ", userDifference);
+                console.log("After question #", (j + 1), "-> new score difference:", userDifference);
             }
-            console.log("Total difference: ", userDifference);
+            console.log("Total difference:", userDifference);
         
             if (userDifference < smallestDifference) {
                 smallestDifference = userDifference;
